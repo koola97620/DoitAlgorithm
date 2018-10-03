36698b4ee646aec52f57b33dddd6f0d610763dbc
Jaeyong <koola97620@nate.com>
2018-10-03 23:56:26 +0900

    09/10/18
      Chapter05 update

--- src/Homework/RecursiveHomework.java
+++ src/Homework/RecursiveHomework.java
@@ -10,7 +10,10 @@ import java.util.Scanner;
 public class RecursiveHomework {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
 
-        
+
+
+
     }
 }
