bf887534b4f0ea53b56a8d1a0ce995b97fe28cd1
Jaeyong <koola97620@nate.com>
2018-10-04 12:08:52 +0900

    recursive update

--- src/Homework/RecursiveHomework.java
+++ src/Homework/RecursiveHomework.java
@@ -10,10 +10,25 @@ import java.util.Scanner;
 public class RecursiveHomework {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int testCase = sc.nextInt();
+        //int testCase = sc.nextInt();
+
+        System.out.println(recur(10));
 
 
 
+
+    }
+
+    public static int recur(int num) {
+        if(num==1) {
+            return 1;
+        }else if(num==2) {
+            return 2;
+        }else if(num==3) {
+            return 4;
+        }else {
+            return recur(num-1) + recur(num-2) + recur(num-3);
+        }
 
     }
 }
